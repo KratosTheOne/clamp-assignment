fe142817a111a2357dc1ff73b90f03ab6e8ad9d5
Kaustubh <kaustubhsinha2001@gmail.com>
2023-04-27 18:38:58 +0530

    commit

--- components/Header.js
+++ components/Header.js
@@ -5,29 +5,31 @@ import down from "../Images/Expand Arrow.png"
 
 const Header = () => {
   return (
-    <div className="flex justify-between text-center border-b-2 px-20 py-2 text-xs font-medium sticky top-0 z-20"> 
-        <div className="py-3">
-            <ul className="flex justify-between items-center">
-                <li className="pr-2">Features</li>
-                <li className="pl-1 pr-2">FAQs</li>
-                <li className="pl-1">Indexes</li>
-            </ul>
-        </div>
-        <div className="py-3">
-            <div>
-                <h4>clamp</h4>
+    <div className="border-b-2">
+        <div className="flex justify-between text-center px-2 py-2 text-xs font-medium sticky top-0 z-20 max-w-7xl mx-auto"> 
+            <div className="py-3">
+                <ul className="flex justify-between items-center">
+                    <li className="pr-2">Features</li>
+                    <li className="pl-1 pr-2">FAQs</li>
+                    <li className="pl-1">Indexes</li>
+                </ul>
             </div>
-        </div>
-        <div className="py-1 flex items-center space-x-3">
-            <div className="flex items-center space-x-1 shadow-md px-2 py-1 font-bold">
-                <Image src={eth} alt="eth-btn" className="h-4 w-4 object-cover bg-gray-200 rounded-full" />
-                <button className="text-xs">Georli</button>
-                <Image src={down} alt="eth-btn" className="h-5 w-5 object-cover" />
+            <div className="py-3">
+                <div>
+                    <h4>clamp</h4>
+                </div>
             </div>
-            <div className="flex items-center space-x-1 shadow-md px-2 py-1 font-bold">
-                <div className="flex space-x-1 bg-gray-100 items-center">
-                    <button className="ml-1">0x5c...d329</button>
+            <div className="py-1 flex items-center space-x-3">
+                <div className="flex items-center space-x-1 shadow-md px-2 py-1 font-bold">
+                    <Image src={eth} alt="eth-btn" className="h-4 w-4 object-cover bg-gray-200 rounded-full" />
+                    <button className="text-xs">Georli</button>
                     <Image src={down} alt="eth-btn" className="h-5 w-5 object-cover" />
+                </div>
+                <div className="flex items-center space-x-1 shadow-md px-2 py-1 font-bold">
+                    <div className="flex space-x-1 bg-gray-100 items-center">
+                        <button className="ml-1">0x5c...d329</button>
+                        <Image src={down} alt="eth-btn" className="h-5 w-5 object-cover" />
+                    </div>
                 </div>
             </div>
         </div>
--- components/Hero.js
+++ components/Hero.js
@@ -1,8 +1,29 @@
+import Image from 'next/image'
 import React from 'react'
+import user from "../Images/Male User.png"
+import info from "../Images/Info.png"
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="border-b-2">
+      <div className="flex max-w-7xl mx-auto px-2">
+        <div className="border-r-2 w-[55%] pt-5 pb-6">
+          <p className="text-lg font-bold">Home</p>
+          <div className="mt-3 flex items-center space-x-3">
+            <Image className="h-16 w-16 object-cover"src={user} alt="user" />
+            <p className="text-lg font-bold">21040xC0...d398</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-lg font-bold pt-5 pl-12">Account Statistics</p>
+          <div className="flex items-center space-x-1 pl-12 pt-2">
+            <p className="text-gray-500 font-medium">Net Worth</p>
+            <Image className="h-3 w-3" src={info} alt="info" />
+          </div>
+          <p className="pl-12 text-3xl font-bold">$509.54</p>
+        </div>
+      </div>  
+    </div>
   )
 }
 
